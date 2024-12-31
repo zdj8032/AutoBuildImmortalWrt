2e6cfd01d02e2f142e8dd3823d0bd0a0480d689b
zdj8032 <113835367+zdj8032@users.noreply.github.com>
2024-12-31 11:56:06 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -23,7 +23,7 @@ done
 if [ "$count" -eq 1 ]; then
   uci set network.lan.proto='dhcp'
 elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.100.1'
+  uci set network.lan.ipaddr='10.10.10.250'
 fi
 
 # 设置所有网口可访问网页终端
@@ -35,7 +35,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by Jed"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
